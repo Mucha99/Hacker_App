f373834fb8146e477919077a28a9c5707c7498fb
Bartek Mucha <bartekmucha43@gmail.com>
2021-08-28 17:57:49 +0200

    Add protection for likes

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,4 +1,5 @@
 const Photo = require('../models/photo.model');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
@@ -61,6 +62,19 @@ exports.vote = async (req, res) => {
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
     if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
+      const voter = await Voter.findOne({ user: req.clientIp });
+      if (!voter) {
+        const newVoter = new Voter({ user: req.clientIp, votes: [photoToUpdate._id]});
+        await newVoter.save();
+      } else {
+        if (voter.votes.includes(photoToUpdate._id)) {
+          throw new Error ('duplicate !'); 
+        } else {
+          voter.votes.push(photoToUpdate._id);
+          await voter.save();
+        }
+      }
+
       photoToUpdate.votes++;
       photoToUpdate.save();
       res.send({ message: 'OK' });
